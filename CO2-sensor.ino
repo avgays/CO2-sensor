3699f7545796233c6ec14e935ffcd596953e4727
avgays <avgays@gmail.com>
2017-04-07 21:04:22 +0300

    code cleanup

--- CO2-sensor.ino
+++ CO2-sensor.ino
@@ -1,5 +1,5 @@
 #define MY_SENSORS
-#define MY_DEBUG
+//#define MY_DEBUG
 #define MY_RADIO_NRF24
 
 #include <SPI.h>
@@ -10,15 +10,10 @@
 #include <MySensors.h>
 #endif
  
-
-
-
-
 /************************Hardware Related Macros************************************/
 #define         MG_PIN                       A0     //define which analog input channel you are going to use
 //#define         BOOL_PIN                     (4)
 //#define         DC_GAIN                      (8.5)   //define the DC gain of amplifier
-#define         DC_GAIN                      (1.0)   //define the DC gain of amplifier
 #define         T_COMP A1 
 #define         CO_VCC 7 
 
@@ -26,45 +21,23 @@
 #define         READ_SAMPLE_INTERVAL         (50)    //define how many samples you are going to take in normal operation
 #define         READ_SAMPLE_TIMES            (5)     //define the time interval(in milisecond) between each samples in 
 
-
-//----PPM - 400/1000-------------------------------------------------
-/**********************Application Related Macros**********************************/
-//These two values differ from sensor to sensor. user should derermine this value.
-#define         ZERO_POINT_VOLTAGE           (1.73) //define the output of the sensor in volts when the concentration of CO2 is 400PPM
-#define         REACTION_VOLTGAE             (0.91) //define the voltage drop of the sensor when move the sensor from air into 1000ppm CO2
-/*****************************Globals***********************************************/
-float           CO2Curve[3] = { 2.602,ZERO_POINT_VOLTAGE,(REACTION_VOLTGAE / (2.602 - 3)) };
-//two points are taken from the curve. 
-//with these two points, a line is formed which is
-//"approximately equivalent" to the original curve.
-//data format:{ x, y, slope}; point1: (lg400, 0.324), point2: (lg4000, 0.280) 
-//slope = ( reaction voltage ) / (log400 –log1000) 
-
-
-//----PPM - 400/10 000-------------------------------------------------
-// float v400ppm = 1.73;   //MUST BE SET ACCORDING TO CALIBRATION
-// float v10000ppm = 0.91; //MUST BE SET ACCORDING TO CALIBRATION````````````````````````
-// float deltavs = v400ppm - v10000ppm;
-// float A = deltavs/(log10(400) - log10(10000));
-// float B = log10(400);
  float v400ppm;   //MUST BE SET ACCORDING TO CALIBRATION
- float v40000ppm; //MUST BE SET ACCORDING TO CALIBRATION````````````````````````
+ float v40000ppm; //MUST BE SET ACCORDING TO CALIBRATION
  float deltavs;
  float A;
  float B = log10(400);
+ bool sendVolts = true;
 
-
+#define CHILD_ID_DIAGNOSTIC 254
 #define CHILD_ID 1   // Id of the sensor child
 #define CHILD_ID_MQ 2   // Id of the sensor child
-#define CHILD_ID_MQ2 4   // Id of the sensor child
-#define CHILD_ID_TEMP 3
-#define CHILD_ID_DIAGNOSTIC 254
+//#define CHILD_ID_TEMP 3
+
 
 #ifdef MY_SENSORS
 MyMessage msg(CHILD_ID, V_VOLTAGE);
 MyMessage msg2(CHILD_ID_MQ, V_LEVEL);
-MyMessage msg3(CHILD_ID_TEMP, V_VOLTAGE);
-MyMessage msg4(CHILD_ID_MQ2, V_LEVEL);
+//MyMessage msg3(CHILD_ID_TEMP, V_VOLTAGE);
 MyMessage msgDiagnostic(CHILD_ID_DIAGNOSTIC, V_CUSTOM);
 #endif
 
@@ -73,20 +46,18 @@ unsigned long SLEEP_TIME;
 void presentation() {
 #ifdef MY_SENSORS
 	String myVals;
-	v400ppm = (float)loadState(1) / 100;
-	v40000ppm = (float)loadState(2) / 100;
+	v400ppm = (float)((int)loadState(2)*256 + loadState(1)) / 1000;
+	v40000ppm = (float)((int)loadState(4) * 256 + loadState(3)) / 1000;
+
 	deltavs = v400ppm - v40000ppm;
 	A = deltavs / (log10(400) - log10(40000));
 	SLEEP_TIME = (unsigned long)(loadState(7)) * 10000;
 	myVals = "0:" + String(v400ppm,2) + ":" + String(v40000ppm,2) + ":0:" + SLEEP_TIME;
 	sendSketchInfo("CO2 sensor", "1.5");
 	present(CHILD_ID, S_MULTIMETER, "Volts");
-	present(CHILD_ID_MQ, S_AIR_QUALITY,"PPM-400/1000");
-	present(CHILD_ID_MQ2, S_AIR_QUALITY,"PPM-400/10 000");
-	present(CHILD_ID_TEMP, S_MULTIMETER, "Temp-comp");
-	//present(CHILD_ID_DIAGNOSTIC, S_CUSTOM, "Diagnostic");
+	present(CHILD_ID_MQ, S_AIR_QUALITY,"CO2 PPM");
+	//present(CHILD_ID_TEMP, S_MULTIMETER, "Temp-comp");
 	present(CHILD_ID_DIAGNOSTIC, S_CUSTOM, myVals.c_str());
-	
 #endif
 #ifdef MY_DEBUG
 	Serial.print("MG-811 Demostration\n");
@@ -95,12 +66,9 @@ void presentation() {
 
 void setup()
 {
-
-
 	analogReference(EXTERNAL);
 	pinMode(CO_VCC, OUTPUT);
 	digitalWrite(CO_VCC, HIGH);
-
 #ifdef MY_DEBUG
 //	Serial.begin(115200);
 //	Serial.println("Power sensor On");
@@ -113,60 +81,44 @@ void setup()
 	Serial.println("V");
 	Serial.print("SLEEP_TIME: ");
 	Serial.println(SLEEP_TIME);
-
-
 	Serial.println("----------------------------");
-
 #endif
 //    pinMode(BOOL_PIN, INPUT);                        //set pin to input
 //    digitalWrite(BOOL_PIN, HIGH);                    //turn on pullup resistors
-               
 }
  
 void loop()
 {
-    int percentage;
-	int percentage2;
+    long percentage;
     float volts;
     float myTemp;
 
 	digitalWrite(CO_VCC, HIGH);
-	//delay(120000);
-	//wait(1000);
-
     //myTemp=analogRead(T_COMP) * 2.5 / 1024;
 
 #ifdef MY_DEB        UG
 //    Serial.print( "Tcomp:" );
 //    Serial.println(myTemp); 
 #endif
-  
     volts = MGRead(MG_PIN);
-	percentage = MGGetPercentage(volts, CO2Curve);
-	percentage2 = MGGetPercentage2(volts);
+	percentage = MGGetPercentage(volts);
 #ifdef MY_DEBUG
-	Serial.print( "SEN0159:" );
+	Serial.print("Volts: " );
     Serial.print(volts); 
     Serial.print( "V           " );
     Serial.print("CO2:");
-    if (percentage == -1) {
-        Serial.print( "<400" );
-    } else {
-        Serial.print(percentage);
-    }
+	Serial.print(percentage);
     Serial.print( "ppm" );
-	Serial.print("       ");
-	Serial.print(percentage2);
-	Serial.print("ppm");
     Serial.print( "       Time point:" );
     Serial.print(millis());
     Serial.print("\n");
 #endif
 #ifdef MY_SENSORS
      send(msg.set(volts,2));
+	 send(msg2.set(percentage));
      //send(msg3.set(myTemp,2));
-     send(msg2.set(percentage));
-	 send(msg4.set(percentage2));
+
+
 #endif
      
 //    if (digitalRead(BOOL_PIN) ){
@@ -175,10 +127,7 @@ void loop()
 //        Serial.print( "=====BOOL is LOW======" );
 //    }
        
-    Serial.print("\n");
-
 	//digitalWrite(CO_VCC, LOW);
-	//wait(120000);
 	wait(SLEEP_TIME);
 }
  
@@ -193,78 +142,72 @@ float MGRead(int mg_pin)
 {
     int i;
     float v=0;
- 
     for (i=0;i<READ_SAMPLE_TIMES;i++) {
         v += analogRead(mg_pin);
         delay(READ_SAMPLE_INTERVAL);
     }
-   // v = (v/READ_SAMPLE_TIMES) *5/1024 ;
 	v = (v / READ_SAMPLE_TIMES) * 2.5 / 1024;
     return v;  
 }
  
-/*****************************  MQGetPercentage **********************************
-Input:   volts   - SEN-000007 output measured in volts
-         pcurve  - pointer to the curve of the target gas
-Output:  ppm of the target gas
-Remarks: By using the slope and a point of the line. The x(logarithmic value of ppm) 
-         of the line could be derived if y(MG-811 output) is provided. As it is a 
-         logarithmic coordinate, power of 10 is used to convert the result to non-logarithmic 
-         value.
-************************************************************************************/
-int  MGGetPercentage(float volts, float *pcurve)
-{
-  
-   if ((volts/DC_GAIN )>=ZERO_POINT_VOLTAGE) {
-      return -1;
-   } else { 
-      return pow(10, ((volts/DC_GAIN)-pcurve[1])/pcurve[2]+pcurve[0]);
-   }
-//  float power = ((volts - v400ppm)/A) + B;
-//  float co2ppm = pow(10,power);
-//  return co2ppm;
-  
-   
-}
 
-int  MGGetPercentage2(float voltage)
-{
+long MGGetPercentage(float voltage) {
 	float power = ((voltage - v400ppm) / A) + B;
-
 	float co2ppm = pow(10, power);
-	return (int)co2ppm;
+	return (long)co2ppm;
 }
 
 void receive(const MyMessage &message) {
 	float myCor = message.getFloat();
+	int myCor2;
+	int newsleep;
+
 	switch (message.type) {
 	case V_VAR1: //v400ppm
 		v400ppm = myCor;
-		saveState(1, (int)(myCor * 100));
+		myCor2 = (int)(myCor * 1000);
+		saveState(1, (byte)(myCor2 & 0xFF));
+		saveState(2, (byte)((myCor2 >> 8) & 0xFF));
 		send(msgDiagnostic.set(myCor, 2));
 		#ifdef MY_DEBUG
 		Serial.print("v400ppm: ");
 		Serial.println(myCor);
 		#endif
 		break;
+
 	case V_VAR2: //v10000ppm
 		v40000ppm = myCor;
-		saveState(2, (int)(myCor * 100));
+		myCor2 = (int)(myCor * 1000);
+		saveState(3, (byte)(myCor2 & 0xFF));
+		saveState(4, (byte)((myCor2 >> 8) & 0xFF));
 		send(msgDiagnostic.set(myCor, 2));
 		#ifdef MY_DEBUG
 		Serial.print("v10000ppm ");
 		Serial.println(myCor);
 		#endif
 		break;
-	case V_VAR4: //V_VAR3 * 10000 - SLEEP_TIME, mls
-		int newsleep = message.getInt();
+
+	case V_VAR4: //V_VAR4 * 10000 - SLEEP_TIME, mls
+		newsleep = message.getInt();
 		SLEEP_TIME = (unsigned long)newsleep * 1000;
 		saveState(7, (int)(newsleep / 10));
 		send(msgDiagnostic.set((uint32_t)SLEEP_TIME));
 		#ifdef MY_DEBUG
-		//Serial.print("SLEEP_TIME ");
-		//Serial.println(SLEEP_TIME);
+		Serial.print("SLEEP_TIME ");
+		Serial.println(SLEEP_TIME);
 		#endif
+		break;
+
+	case V_VAR5: //sendVolts
+		sendVolts = message.getBool();
+		saveState(8, (byte)(sendVolts));
+		send(msgDiagnostic.set((int)(sendVolts)));
+		#ifdef MY_DEBUG
+		Serial.print("sendVolts ");
+		Serial.println(sendVolts);
+		#endif
+		break;
+	default:
 		break;
 	}
 	deltavs = v400ppm - v40000ppm;
--- Release/CO2-sensor.cpp.d
+++ Release/CO2-sensor.cpp.d
@@ -1,49 +1,48 @@
-C:\Users\avgays\AppData\Local\Temp\VMBuilds\CO2-sensor\nano_atmega328\Release\CO2-sensor.cpp.o: \
- C:\Users\avgays\AppData\Local\Temp\VMBuilds\CO2-sensor\nano_atmega328\Release\CO2-sensor.cpp \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\eightanaloginputs/pins_arduino.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\eightanaloginputs/../standard/pins_arduino.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\SPI\src/SPI.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\Wire\src/Wire.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
- C:\Users\avgays\Documents\Arduino\libraries\MySensors/MySensors.h \
- C:\Users\avgays\Documents\Arduino\libraries\MySensors/core/MySensorsCore.h \
- C:\Users\avgays\Documents\Arduino\libraries\MySensors/core/Version.h \
- C:\Users\avgays\Documents\Arduino\libraries\MySensors/MyConfig.h \
- C:\Users\avgays\Documents\Arduino\libraries\MySensors/core/MyEepromAddresses.h \
- C:\Users\avgays\Documents\Arduino\libraries\MySensors/core/MyMessage.h \
- C:\Users\avgays\Documents\Arduino\libraries\MySensors/drivers/AVR/DigitalWriteFast/digitalWriteFast.h \
- C:\Users\avgays\Documents\Arduino\libraries\MySensors/core/MyHwAVR.cpp \
- C:\Users\avgays\Documents\Arduino\libraries\MySensors/core/MyHwAVR.h \
- C:\Users\avgays\Documents\Arduino\libraries\MySensors/core/MyHw.h \
- C:\Users\avgays\Documents\Arduino\libraries\MySensors/core/MyLeds.h \
- C:\Users\avgays\Documents\Arduino\libraries\MySensors/core/MyIndication.cpp \
- C:\Users\avgays\Documents\Arduino\libraries\MySensors/core/MyIndication.h \
- C:\Users\avgays\Documents\Arduino\libraries\MySensors/core/MySigning.cpp \
- C:\Users\avgays\Documents\Arduino\libraries\MySensors/core/MySigning.h \
- C:\Users\avgays\Documents\Arduino\libraries\MySensors/core/MySensorsCore.h \
- C:\Users\avgays\Documents\Arduino\libraries\MySensors/drivers/ATSHA204/ATSHA204.h \
- C:\Users\avgays\Documents\Arduino\libraries\MySensors/drivers/ATSHA204/sha256.h \
- C:\Users\avgays\Documents\Arduino\libraries\MySensors/drivers/ATSHA204/sha256.cpp \
- C:\Users\avgays\Documents\Arduino\libraries\MySensors/drivers/ATSHA204/sha256.h \
- C:\Users\avgays\Documents\Arduino\libraries\MySensors/core/MyTransport.cpp \
- C:\Users\avgays\Documents\Arduino\libraries\MySensors/core/MyTransport.h \
- C:\Users\avgays\Documents\Arduino\libraries\MySensors/drivers/RF24/RF24.cpp \
- C:\Users\avgays\Documents\Arduino\libraries\MySensors/drivers/RF24/RF24.h \
- C:\Users\avgays\Documents\Arduino\libraries\MySensors/core/MyTransportNRF24.cpp \
- C:\Users\avgays\Documents\Arduino\libraries\MySensors/drivers/RF24/RF24.h \
- C:\Users\avgays\Documents\Arduino\libraries\MySensors/drivers/CircularBuffer/CircularBuffer.h \
- C:\Users\avgays\Documents\Arduino\libraries\MySensors/core/MyCapabilities.h \
- C:\Users\avgays\Documents\Arduino\libraries\MySensors/core/MyMessage.cpp \
- C:\Users\avgays\Documents\Arduino\libraries\MySensors/core/MySensorsCore.cpp \
- C:\Users\avgays\Documents\Arduino\libraries\MySensors/core/MyMainDefault.cpp
+C:\Users\avgays.LOCAL\AppData\Local\Temp\VMBuilds\CO2-sensor\MYSBL13_16MHzatmega328\Release\CO2-sensor.cpp.o: \
+ C:\Users\avgays.LOCAL\AppData\Local\Temp\VMBuilds\CO2-sensor\MYSBL13_16MHzatmega328\Release\CO2-sensor.cpp \
+ C:\Users\avgays.LOCAL\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.17\cores\arduino/arduino.h \
+ C:\Users\avgays.LOCAL\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.17\cores\arduino/binary.h \
+ C:\Users\avgays.LOCAL\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.17\cores\arduino/WCharacter.h \
+ C:\Users\avgays.LOCAL\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.17\cores\arduino/WString.h \
+ C:\Users\avgays.LOCAL\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.17\cores\arduino/HardwareSerial.h \
+ C:\Users\avgays.LOCAL\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.17\cores\arduino/Stream.h \
+ C:\Users\avgays.LOCAL\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.17\cores\arduino/Print.h \
+ C:\Users\avgays.LOCAL\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.17\cores\arduino/Printable.h \
+ C:\Users\avgays.LOCAL\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.17\cores\arduino/USBAPI.h \
+ C:\Users\avgays.LOCAL\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.17\cores\arduino/Arduino.h \
+ C:\Users\avgays.LOCAL\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.17\variants\standard/pins_arduino.h \
+ C:\Users\avgays.LOCAL\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.17\libraries\SPI\src/SPI.h \
+ C:\Users\avgays.LOCAL\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.17\cores\arduino/Arduino.h \
+ C:\Users\avgays.LOCAL\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.17\libraries\Wire\src/Wire.h \
+ C:\Users\avgays.LOCAL\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.17\cores\arduino/Stream.h \
+ C:\Users\avgays.LOCAL\Documents\Arduino\libraries\MySensors/MySensors.h \
+ C:\Users\avgays.LOCAL\Documents\Arduino\libraries\MySensors/core/MySensorsCore.h \
+ C:\Users\avgays.LOCAL\Documents\Arduino\libraries\MySensors/core/Version.h \
+ C:\Users\avgays.LOCAL\Documents\Arduino\libraries\MySensors/MyConfig.h \
+ C:\Users\avgays.LOCAL\Documents\Arduino\libraries\MySensors/core/MyEepromAddresses.h \
+ C:\Users\avgays.LOCAL\Documents\Arduino\libraries\MySensors/core/MyMessage.h \
+ C:\Users\avgays.LOCAL\Documents\Arduino\libraries\MySensors/drivers/AVR/DigitalWriteFast/digitalWriteFast.h \
+ C:\Users\avgays.LOCAL\Documents\Arduino\libraries\MySensors/core/MyHwAVR.cpp \
+ C:\Users\avgays.LOCAL\Documents\Arduino\libraries\MySensors/core/MyHwAVR.h \
+ C:\Users\avgays.LOCAL\Documents\Arduino\libraries\MySensors/core/MyHw.h \
+ C:\Users\avgays.LOCAL\Documents\Arduino\libraries\MySensors/core/MyLeds.h \
+ C:\Users\avgays.LOCAL\Documents\Arduino\libraries\MySensors/core/MyIndication.cpp \
+ C:\Users\avgays.LOCAL\Documents\Arduino\libraries\MySensors/core/MyIndication.h \
+ C:\Users\avgays.LOCAL\Documents\Arduino\libraries\MySensors/core/MySigning.cpp \
+ C:\Users\avgays.LOCAL\Documents\Arduino\libraries\MySensors/core/MySigning.h \
+ C:\Users\avgays.LOCAL\Documents\Arduino\libraries\MySensors/core/MySensorsCore.h \
+ C:\Users\avgays.LOCAL\Documents\Arduino\libraries\MySensors/drivers/ATSHA204/ATSHA204.h \
+ C:\Users\avgays.LOCAL\Documents\Arduino\libraries\MySensors/drivers/ATSHA204/sha256.h \
+ C:\Users\avgays.LOCAL\Documents\Arduino\libraries\MySensors/drivers/ATSHA204/sha256.cpp \
+ C:\Users\avgays.LOCAL\Documents\Arduino\libraries\MySensors/drivers/ATSHA204/sha256.h \
+ C:\Users\avgays.LOCAL\Documents\Arduino\libraries\MySensors/core/MyTransport.cpp \
+ C:\Users\avgays.LOCAL\Documents\Arduino\libraries\MySensors/core/MyTransport.h \
+ C:\Users\avgays.LOCAL\Documents\Arduino\libraries\MySensors/drivers/RF24/RF24.cpp \
+ C:\Users\avgays.LOCAL\Documents\Arduino\libraries\MySensors/drivers/RF24/RF24.h \
+ C:\Users\avgays.LOCAL\Documents\Arduino\libraries\MySensors/core/MyTransportNRF24.cpp \
+ C:\Users\avgays.LOCAL\Documents\Arduino\libraries\MySensors/drivers/RF24/RF24.h \
+ C:\Users\avgays.LOCAL\Documents\Arduino\libraries\MySensors/drivers/CircularBuffer/CircularBuffer.h \
+ C:\Users\avgays.LOCAL\Documents\Arduino\libraries\MySensors/core/MyCapabilities.h \
+ C:\Users\avgays.LOCAL\Documents\Arduino\libraries\MySensors/core/MyMessage.cpp \
+ C:\Users\avgays.LOCAL\Documents\Arduino\libraries\MySensors/core/MySensorsCore.cpp \
+ C:\Users\avgays.LOCAL\Documents\Arduino\libraries\MySensors/core/MyMainDefault.cpp
